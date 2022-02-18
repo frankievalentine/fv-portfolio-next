1c0355e6cf03950b61f8d0cfd3bf8a954572a2b9
Frankie Valentine <frankievalentine9@gmail.com>
2022-02-17 22:41:09 -0800

    Added Notion: -db -token -lib -css modules for blog and post
    Added favicon
    Changed prettier config

---
+++ pages/blog/index.js
@@ -0,0 +1,65 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { getDatabase } from '../../lib/notion';
+import { Text } from './[id].js';
+import styles from './blogindex.module.css';
+
+export const databaseId = process.env.NOTION_DATABASE_ID;
+
+export default function Home({ posts }) {
+  return (
+    <div>
+      <Head>
+        <title>Notion Next.js blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <header className={styles.header}>
+          <h1>Next.js blog powered by Notion API</h1>
+        </header>
+
+        <h2 className={styles.heading}>All Posts</h2>
+        <ol className={styles.posts}>
+          {posts.map((post) => {
+            const date = new Date(post.last_edited_time).toLocaleString(
+              'en-US',
+              {
+                month: 'short',
+                day: '2-digit',
+                year: 'numeric',
+              }
+            );
+            return (
+              <li key={post.id} className={styles.post}>
+                <h3 className={styles.postTitle}>
+                  <Link href={`/${post.id}`}>
+                    <a>
+                      <Text text={post.properties.Name.title} />
+                    </a>
+                  </Link>
+                </h3>
+
+                <p className={styles.postDescription}>{date}</p>
+                <Link href={`/blog/${post.id}`}>
+                  <a>Read post →</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ol>
+      </main>
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
+
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
