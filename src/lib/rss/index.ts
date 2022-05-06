7dbe936f7877ac801829527271233823997b5bf1
Frankie Valentine <frankievalentine9@gmail.com>
2022-05-06 16:52:29 -0700

    Remove posts from feed

--- src/lib/rss/index.ts
+++ src/lib/rss/index.ts
@@ -22,41 +22,41 @@ const feed = new Feed({
   },
 });
 
-posts.forEach((post) => {
-  feed.addItem({
-    title: post.title,
-    id: post.url,
-    link: post.url,
-    description: post.description,
-    content: post.content,
-    author: [
-      {
-        name: 'Jane Doe',
-        email: 'janedoe@example.com',
-        link: 'https://example.com/janedoe',
-      },
-      {
-        name: 'Joe Smith',
-        email: 'joesmith@example.com',
-        link: 'https://example.com/joesmith',
-      },
-    ],
-    contributor: [
-      {
-        name: 'Shawn Kemp',
-        email: 'shawnkemp@example.com',
-        link: 'https://example.com/shawnkemp',
-      },
-      {
-        name: 'Reggie Miller',
-        email: 'reggiemiller@example.com',
-        link: 'https://example.com/reggiemiller',
-      },
-    ],
-    date: post.date,
-    image: post.image,
-  });
-});
+// posts.forEach((post) => {
+//   feed.addItem({
+//     title: post.title,
+//     id: post.url,
+//     link: post.url,
+//     description: post.description,
+//     content: post.content,
+//     author: [
+//       {
+//         name: 'Jane Doe',
+//         email: 'janedoe@example.com',
+//         link: 'https://example.com/janedoe',
+//       },
+//       {
+//         name: 'Joe Smith',
+//         email: 'joesmith@example.com',
+//         link: 'https://example.com/joesmith',
+//       },
+//     ],
+//     contributor: [
+//       {
+//         name: 'Shawn Kemp',
+//         email: 'shawnkemp@example.com',
+//         link: 'https://example.com/shawnkemp',
+//       },
+//       {
+//         name: 'Reggie Miller',
+//         email: 'reggiemiller@example.com',
+//         link: 'https://example.com/reggiemiller',
+//       },
+//     ],
+//     date: post.date,
+//     image: post.image,
+//   });
+// });
 
 feed.addCategory('Technologie');
 
